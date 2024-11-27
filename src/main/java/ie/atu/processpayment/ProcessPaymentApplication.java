047ec1572fe6e9db5997cc79394cb8fdc85def6c
G00413711@atu.ie <G00413711@atu.ie>
2024-11-27 14:03:48 +0000

    Created project

---
+++ src/main/java/ie/atu/processpayment/ProcessPaymentApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.processpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessPaymentApplication.class, args);
+    }
+
+}
